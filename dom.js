70883aba15799a6cea13ee6c1521ccedff500fcb
Rakshit Agarwal <rakshitag13@gmail.com>
2022-11-08 17:26:59 +0530

    queryselectorall\

--- dom.js
+++
@@ -1,9 +0,0 @@
-var items = document.getElementsByClassName('list-group-item');
-
-var li =document.getElementsByTagName('li');
-
-items[2].style.backgroundColor = "Green";
-
-for(var i=0; i< li.length; i++){
-    li[i].style.fontWeight = "bold";
-}
