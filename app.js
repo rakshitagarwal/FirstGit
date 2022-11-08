1347d4cac1370dc947de8dd761dd64b1297b2408
Rakshit Agarwal <rakshitag13@gmail.com>
2022-11-08 18:05:35 +0530

    task 12

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 //Local Storage
 //localStoraget-->setItem(key:value),getItem(key) all in strings, removeItem(key)
 
-const userName = document.querySelector("#userName");
-const email = document.querySelector("#email");
 const form = document.querySelector("#new-form");
-let arr = []; //used for storing userObj
+
+if (JSON.parse(localStorage.getItem("userDetails")).length) {
+  const userDetailsDeserialized = JSON.parse(
+    localStorage.getItem("userDetails")
+  );
+  userDetailsDeserialized.forEach((obj) => {
+    display(obj);
+  });
+}
+
+//let arr = []; //used for storing userObj
 const onsubmit = (e) => {
   //e.target is the form
   e.preventDefault();
+  const userDetails = JSON.parse(localStorage.getItem("userDetails")) || []; //important
+  const userName = document.querySelector("#userName");
+  const email = document.querySelector("#email");
   if (userName.value == "" || email.value == "") {
     const msg = document.querySelector(".msg");
     msg.appendChild(document.createTextNode("Please enter all details"));
@@ -21,25 +32,33 @@ const onsubmit = (e) => {
       name: userName.value,
       email: email.value,
     };
-    arr.push(usrObj);
-    let arr_serialized = JSON.stringify(); // serialized-->convert the object to JSON
-    localStorage.setItem("userDetails", arr_serialized);
+    userDetails.push(usrObj);
+    let arr_Serialized = JSON.stringify(userDetails); // serialized-->convert the object to JSON
+    localStorage.setItem("userDetails", arr_Serialized);
+
+    display(usrObj);
     userName.value = "";
     email.value = "";
   }
 };
 
+function display(obj) {
+  console.log(obj);
+  const ul = document.querySelector(".unordered-list");
+  const li = document.createElement("li");
+  const btnEdit = document.createElement("button");
+  const btnDel = document.createElement("button");
+  btnEdit.className = "edit";
+  btnDel.className = "del";
+  btnEdit.innerText = "Edit";
+  btnDel.innerText = "Delete";
+
+  li.className = "list";
+  li.appendChild(document.createTextNode(`${obj.name}: ${obj.email}`));
+  li.appendChild(btnDel);
+  li.appendChild(btnEdit);
+  ul.insertAdjacentElement("beforeend", li);
+}
+
 form.addEventListener("submit", onsubmit);
-
-/*
-for sharpner task:
-const usrObj = {
-  name: userName.value,
-  email: email.value,
-};
-const userObjSerialized=SON.stringify(usrObj);
-localStorage.setItem("users", arr_serialized);
-
-to convert object to JSON object--->JSON.stringify(userObj)
-to convert JSON to regular object--->JSON.parse(userObjSerialized)
-*/
+// console.log(JSON.parse(localStorage.getItem("userDetails")));
