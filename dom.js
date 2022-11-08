f92eddd027933de38419e07027f3edc189e1d478
Rakshit Agarwal <rakshitag13@gmail.com>
2022-11-08 16:46:47 +0530

    getelementbytagname

--- dom.js
+++ dom.js
@@ -1,9 +1,9 @@
 var items = document.getElementsByClassName('list-group-item');
 
-console.log(items);
+var li =document.getElementsByTagName('li');
 
 items[2].style.backgroundColor = "Green";
 
-for(var i=0; i< items.length; i++){
-    items[i].style.fontWeight = "bold";
+for(var i=0; i< li.length; i++){
+    li[i].style.fontWeight = "bold";
 }
