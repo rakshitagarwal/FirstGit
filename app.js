dfc7e0a549b101c25686affbfd3bc7f3176b4010
Rakshit Agarwal <rakshitag13@gmail.com>
2022-11-08 18:30:37 +0530

    task 13

--- app.js
+++ app.js
@@ -1,49 +1,6 @@
-//Local Storage
-//localStoraget-->setItem(key:value),getItem(key) all in strings, removeItem(key)
-
 const form = document.querySelector("#new-form");
-
-if (JSON.parse(localStorage.getItem("userDetails")).length) {
-  const userDetailsDeserialized = JSON.parse(
-    localStorage.getItem("userDetails")
-  );
-  userDetailsDeserialized.forEach((obj) => {
-    display(obj);
-  });
-}
-
-//let arr = []; //used for storing userObj
-const onsubmit = (e) => {
-  //e.target is the form
-  e.preventDefault();
-  const userDetails = JSON.parse(localStorage.getItem("userDetails")) || []; //important
-  const userName = document.querySelector("#userName");
-  const email = document.querySelector("#email");
-  if (userName.value == "" || email.value == "") {
-    const msg = document.querySelector(".msg");
-    msg.appendChild(document.createTextNode("Please enter all details"));
-    msg.className = "msg-text";
-    setTimeout(() => {
-      msg.style.display = "none";
-    }, 2000);
-  } else {
-    console.log("going to local");
-    const usrObj = {
-      name: userName.value,
-      email: email.value,
-    };
-    userDetails.push(usrObj);
-    let arr_Serialized = JSON.stringify(userDetails); // serialized-->convert the object to JSON
-    localStorage.setItem("userDetails", arr_Serialized);
-
-    display(usrObj);
-    userName.value = "";
-    email.value = "";
-  }
-};
-
-function display(obj) {
-  console.log(obj);
+const displayOnPage = (obj) => {
+  // console.log(obj);
   const ul = document.querySelector(".unordered-list");
   const li = document.createElement("li");
   const btnEdit = document.createElement("button");
@@ -52,13 +9,70 @@ function display(obj) {
   btnDel.className = "del";
   btnEdit.innerText = "Edit";
   btnDel.innerText = "Delete";
+  li.addEventListener("click", (e) => {
+    if (e.target.className == "del") {
+      const userDetails = e.target.parentElement.childNodes[0].textContent; //get the string inside the li
+      Object.keys(localStorage).forEach((key) => {
+        if (userDetails.indexOf(`${key}`) != -1) {
+          localStorage.removeItem(key);
+          e.target.parentElement.remove();
+        }
+      });
+    } else if (e.target.className == "edit") {
+      const name = document.querySelector("#userName");
+      const email = document.querySelector("#email");
+      const userDetails = e.target.parentElement.childNodes[0].textContent;
+      // console.log(userDetails);
+      Object.keys(localStorage).forEach((key) => {
+        if (userDetails.indexOf(`${key}`) != -1) {
+          name.value = JSON.parse(localStorage.getItem(key)).name;
+          email.value = JSON.parse(localStorage.getItem(key)).email;
+          localStorage.removeItem(key);
+          e.target.parentElement.remove();
+        }
+      });
+    }
+  });
 
   li.className = "list";
-  li.appendChild(document.createTextNode(`${obj.name}: ${obj.email}`));
+  li.appendChild(document.createTextNode(`${obj.name} : ${obj.email}`));
   li.appendChild(btnDel);
   li.appendChild(btnEdit);
   ul.insertAdjacentElement("beforeend", li);
-}
+};
 
+const pageReload = () => {
+  const userDetailsKeys = Object.keys(localStorage);
+  userDetailsKeys.forEach((key) => {
+    displayOnPage(JSON.parse(localStorage.getItem(key))); //each user object
+  });
+};
+
+const onsubmit = (e) => {
+  e.preventDefault();
+  const name = document.querySelector("#userName");
+  const email = document.querySelector("#email");
+  if (name.value == "" || email.value == "") {
+    const msg = document.querySelector(".msg");
+    msg.style.display = "block";
+    setTimeout(() => {
+      msg.style.display = "none";
+    }, 2000);
+  } else {
+    const ul = document.querySelector(".unordered-list");
+    Array.from(ul.children).forEach((li) => {
+      if (li.childNodes[0].textContent.indexOf(email.value) > -1) {
+        li.remove();
+      }
+    });
+    const user = { name: name.value, email: email.value };
+    localStorage.setItem(`${email.value}`, JSON.stringify(user));
+    name.value = "";
+    email.value = "";
+
+    displayOnPage(user);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", pageReload); //DOMContentLoaded is fired when page is reloded
 form.addEventListener("submit", onsubmit);
-// console.log(JSON.parse(localStorage.getItem("userDetails")));
